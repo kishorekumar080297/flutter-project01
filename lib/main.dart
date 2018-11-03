fc419d5d5da2a7d72b71f8ac144574a1b2c1341b
Kishore Kumar G <kishorekumar080297@gmail.com>
2018-11-03 21:54:41 +0530

    Added appBar and Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,18 @@ import 'package:flutter/material.dart';
 //TODO: create a main Function
 void main() {
   //TODO: Create a widget
+  
+  final barColor = const Color(0xFFFAD02E);
+  final bgColor = const Color(0xFF333945);
 
-  var app = MaterialApp(home: Text('Hi there, Kishore from Sify'));
+  var app = MaterialApp(
+    home: Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        backgroundColor: barColor,
+      ),
+    ),
+    );
 
 //TODO: Run that widget on mobile device
 
