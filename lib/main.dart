6e77d0f3705ddfec6162147fbf925250975fce40
Kishore Kumar G <kishorekumar080297@gmail.com>
2018-11-12 18:25:08 +0530

    Made the app modular

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,14 @@
 //Created by Kishore Kumar Govindaradjou on 03/10/2018
 //TODO: import library
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 //TODO: create a main Function
 void main() {
-  //TODO: Create a widget
-  
-  final barColor = const Color(0xFFFAD02E);
-  final bgColor = const Color(0xFF333945);
-
-  var app = MaterialApp(
-    home: Scaffold(
-      backgroundColor: bgColor,
-      appBar: AppBar(
-        title: Text('Instagram', style: TextStyle(color: bgColor, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold)),
-        backgroundColor: barColor,
-      ),
-      
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add, color: bgColor, size: 30),
-        backgroundColor: barColor,
-        onPressed: () {}
-      ),
-    ),
-    );
 
 //TODO: Run that widget on mobile device
 
-runApp(app);
+runApp(App());
 }
 
 
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
+  final barColor = const Color(0xFFFAD02E);
+  final bgColor = const Color(0xFF333945);
+  Widget build(context) {
+    return MaterialApp(
+    home: Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        title: Text('Instagram', style: TextStyle(color: bgColor, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold)),
+        backgroundColor: barColor,
+      ),
+      
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add, color: bgColor, size: 30),
+        backgroundColor: barColor,
+        onPressed: () {}
+      ),
+    ),
+    );
+  }
 }
